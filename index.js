84bf98c9277c9693fc99d5f95c719640912d18e6
hurelu <hugov@runbox.com>
2015-11-22 18:25:21 -0600

    lint and minor fix

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-function sumProduct(array) {  // can use any number of arrays
-	var sum = 0
-	var N = Array.isArray(array) ? array.length : 0
-	for (var i=0, args=[]; i<arguments.length; i++) {
-		// if arrays don't have the same length, we use the shortest one
-		if (!Array.isArray(arguments[i])) return NaN
-		args[i] = arguments[i]
-		if (args[i].length < N) N = args[i].length
-	}
+function sumProduct (array) {  // can use any number of arrays
+  var sum = 0
+  var N = Array.isArray(array) ? array.length : 0
+  for (var i = 0, args = []; i < arguments.length; i++) {
+    // if arrays don't have the same length, we use the shortest one
+    if (!Array.isArray(arguments[i])) return NaN
+    args[i] = arguments[i]
+    if (args[i].length < N) N = args[i].length
+  }
 
-	for (var i=0; i<N; i++) {
-		var prod = 1
-		for (var j=0; j<args.length; j++) prod *= args[j][i]
-		sum += prod
-	}
-	return Number(sum)
+  for (i = 0; i < N; i++) {
+    var prod = 1
+    for (var j = 0; j < args.length; j++) prod *= args[j][i]
+    sum += prod
+  }
+  return Number(sum)
 }
-
 
 module.exports = sumProduct
--- test.js
+++ test.js
@@ -1,21 +1,17 @@
 'use strict'
-var test = require('tape')
+var test = require('tt')
 // var t = require('assert')
 var sumProduct = require('./index.js')
 
+test('sum-product', function (t) {
+  t.equal(sumProduct(), 0, 'should be 0 if no inputs provided')
+  t.ok(isNaN(sumProduct(1, 2)), 'should be NaN if inputs are not arrays')
+  t.ok(isNaN(sumProduct([], 2)), 'should be NaN if inputs are not arrays')
+  t.equal(sumProduct([], []), 0, 'should be 0 if inputs are empty')
+  t.equal(sumProduct([1], [1]), 1, 'should work with unit arrays')
+  t.equal(sumProduct([1, 1]), 2, 'should work with a single array')
+  t.equal(sumProduct([1, 1], [1, 1], [1, 1], [1, 1], [1, 1]), 2, 'should work with 2 arrays')
+  t.equal(sumProduct([1, 1], [1, 1], [1, 1], [1, 1], [1, 1]), 2, 'should work with many arrays')
 
-test('sum-product', function(t) {
-// t.comment('*****')
-console.log( sumProduct() )
-	t.equal( sumProduct(), 0, 'should be 0 if no inputs provided' )
-	// t.comment('*****')
-	t.true( isNaN( sumProduct(1,2) ), 'should be NaN if inputs are not arrays' )
-	t.true( isNaN( sumProduct([],2) ), 'should be NaN if inputs are not arrays' )
-	t.equal( sumProduct([],[]), 0, 'should be 0 if inputs are empty' )
-	t.equal( sumProduct([1],[1]), 1, 'should work with unit arrays' )
-	t.equal( sumProduct([1,1]), 2, 'should work with a single array' )
-	t.equal( sumProduct([1,1],[1,1],[1,1],[1,1],[1,1]), 2, 'should work with 2 arrays' )
-	t.equal( sumProduct([1,1],[1,1],[1,1],[1,1],[1,1]), 2, 'should work with many arrays' )
-
-	t.end()
+  t.end()
 })
