c1bde50fe7783167c2b438561b0a7bf9a53ccd4a
hville <hugov@runbox.com>
2016-09-18 01:27:20 +0800

    readme fix

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-function sumProduct (array) {  // can use any number of arrays
-  var sum = 0
-  var N = Array.isArray(array) ? array.length : 0
-  for (var i = 0, args = []; i < arguments.length; i++) {
-    // if arrays don't have the same length, we use the shortest one
-    if (!Array.isArray(arguments[i])) return NaN
-    args[i] = arguments[i]
-    if (args[i].length < N) N = args[i].length
-  }
-
-  for (i = 0; i < N; i++) {
-    var prod = 1
-    for (var j = 0; j < args.length; j++) prod *= args[j][i]
-    sum += prod
-  }
-  return Number(sum)
-}
-
 module.exports = sumProduct
+
+/**
+ * @param  {array} array - first source array
+ * @param  {array} [other] - any number of additional arrays
+ * @return {number} sum of the product
+ */
+function sumProduct (array /*other arrays*/) {
+	if (!arguments.length) return 0
+	if (!Array.isArray(array)) return NaN
+	var dim = arguments.length,
+			len = array.length,
+			args = [array],
+			sum = 0,
+			err = 0,
+			tot = 0
+
+	for (var i = 1; i < dim; ++i) {
+		if (!Array.isArray(arguments[i]) || arguments[i].length !== len) return NaN
+		args[i] = arguments[i]
+	}
+
+	//modified Kahan Sum
+	for (i = 0; i < len; ++i) {
+		var prod = 1
+		for (var j = 0; j < args.length; ++j) prod *= args[j][i]
+		sum += prod
+		err += (sum - tot) - prod
+		tot = sum
+	}
+	return sum - err
+}
--- test.js
+++ test.js
@@ -1,17 +1,27 @@
 'use strict'
-var test = require('tt')
-// var t = require('assert')
+var c = require('cotest')
 var sumProduct = require('./index.js')
 
-test('sum-product', function (t) {
-  t.equal(sumProduct(), 0, 'should be 0 if no inputs provided')
-  t.ok(isNaN(sumProduct(1, 2)), 'should be NaN if inputs are not arrays')
-  t.ok(isNaN(sumProduct([], 2)), 'should be NaN if inputs are not arrays')
-  t.equal(sumProduct([], []), 0, 'should be 0 if inputs are empty')
-  t.equal(sumProduct([1], [1]), 1, 'should work with unit arrays')
-  t.equal(sumProduct([1, 1]), 2, 'should work with a single array')
-  t.equal(sumProduct([1, 1], [1, 1], [1, 1], [1, 1], [1, 1]), 2, 'should work with 2 arrays')
-  t.equal(sumProduct([1, 1], [1, 1], [1, 1], [1, 1], [1, 1]), 2, 'should work with many arrays')
-
-  t.end()
+c('sum-product, input types', function () {
+	c('===', sumProduct(), 0) // should be 0 if no inputs provided
+	c('===', isNaN(sumProduct(1, 2)), true) // should be NaN if inputs are not arrays
+	c('===', isNaN(sumProduct([], 2)), true) // should be NaN if inputs are not arrays
+	c('===', sumProduct([], []), 0) // should be 0 if inputs are empty
+	c('===', sumProduct([1], [1]), 1) // should work with unit arrays
+	c('===', sumProduct([1, 1]), 2) // should work with a single array
+	c('===', sumProduct([1, 1], [1, 1]), 2) // should work with 2 arrays
+	c('===', sumProduct([1, 1], [1, 1], [1, 1], [1, 1], [1, 1]), 2) // should work with many arrays
+})
+c('sum-product, results', function () {
+	c('===', sumProduct([1,1],[1,1],[1,1]), 2)
+	c('===', sumProduct([-1,1],[1,1],[1,1]), 0)
+	c('===', sumProduct([-1,1],[1,-1],[1,-1]), 0)
+})
+c('handle destructive error cancellation', function () {
+	var epsilon = Number.Epsilon || Math.pow(2, -52)
+	c('===', sumProduct([1 + epsilon, 1, 1 + epsilon],[1, 1 + epsilon, -1]), 1 + epsilon)
+	c('===', sumProduct([1, -0.01],[0.1,0.1],[0.01,1]), 0)
+	c('===', sumProduct([1, -0.01],[0.1, -0.1],[0.01,1]), 0.002)
+	c('<', sumProduct([0.1, -0.1],[-0.2, 0.2],[0.3, 0.3]) -0.012, epsilon)
+	c('<', sumProduct([0.1, 0.1],[0.2, -0.2],[0.3, -0.3]) - 0.012, epsilon)
 })
